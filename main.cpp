eed4e805c58892ed1706fe529aece92e65cdb7b2
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2021-09-11 03:20:01 +0530

    Completed File

--- main.cpp
+++ main.cpp
@@ -6,13 +6,45 @@ using namespace std;
 int main()
 {
     unordered_map<string,int> m;
-    m["gfg"] = 20;
-    m["ide"] = 30;
-    m.insert({"courses", 15});
+    m["Narendra"] = 20;
+    m["Jha"] = 30;
+
+    //INSERT
+
+    m.insert({"Hobbies", 15});
 
     for(auto x: m){
         cout<<x.first<<" "<<x.second<<endl;
     }
+
+
+    //FIND(), BEGIN() & END()
+    //m.find() will return iterator to the element while m.end  returns an iterator pointing to the position past the last element in the container in the unordered_map container. 
+
+
+    if(m.find("Narendra")!=m.end())
+        cout<<"Found"<<endl;
+    else
+        cout<<"Not Found"<<endl;
+
+    for(auto it=m.begin();it!=m.end();it++)
+        cout<<(it->first)<<" "<<(it->second)<<endl;
+
+    //COUNT()
+
+
+    if(m.count("Narendra")>0)
+        cout<<"found"<<endl;
+    else
+        cout<<"not found"<<endl;
+
+    //ERASE() & SIZE()
+
+
+    cout<<"Size "<< m.size() <<endl;
+    m.erase("Narendra");
+    m.erase(m.begin(),m.end());
+    cout<<"Size "<<m.size()<<endl;
 
     return 0;
 }
